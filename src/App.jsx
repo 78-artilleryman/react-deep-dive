be06681078c822ad0885fd26b7a0466325b43db2
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-01-11 21:38:35 +0900

    ✨ Feat: react createElement 함수 구현
    feat: react createElement 함수 구현

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "../lib/React";
+
+function App() {
+  return (
+    <div id="app">
+      <header>
+        <h1>Hello world</h1>
+      </header>
+      <body>
+        <p>test</p>
+        <p>윤병현</p>
+      </body>
+      <footer>
+        <p>bottom</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
