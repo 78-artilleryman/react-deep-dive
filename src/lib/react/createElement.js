09bba6a0f7db2e33864b598e9bb23f6a3b8a75dc
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-01-15 20:41:05 +0900

    🔥 Remove: 필요없는 코드 제거
    remove: 필요없는 코드 제거

--- src/lib/react/createElement.js
+++ src/lib/react/createElement.js
@@ -2,7 +2,6 @@ export default function createElement(type, props, ...children) {
   if (typeof type === "function") {
     return type({ ...props, children });
   }
-
   // props가 null이면 빈 객체로 초기화
   const normalizedProps = props || null;
 
@@ -10,15 +9,5 @@ export default function createElement(type, props, ...children) {
     type,
     props: normalizedProps,
     children: children,
-  };
-}
-
-export function createTextElement(text) {
-  return {
-    type: "TEXT_ELEMENT",
-    props: {
-      nodeValue: text,
-      children: [],
-    },
   };
 }
